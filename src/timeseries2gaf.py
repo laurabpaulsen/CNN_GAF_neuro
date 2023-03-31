484aaf49356d8036f89a10a5c054c6e19a1dc6c1
“laurabpauslen” <202005791@post.au.dk>
2023-03-31 18:32:30 +0200

    remove unused imports

--- src/timeseries2gaf.py
+++ src/timeseries2gaf.py
@@ -6,12 +6,7 @@
 import numpy as np
 from pyts.image import GramianAngularField
 from pathlib import Path
-import math
-import dtcwt
-from dtcwt import compat as cp
     
-
-
 
 def main():
     path = Path(__file__)
@@ -21,7 +16,7 @@ def main():
     X = np.load(timeseries_path)
     y = np.load(labels_path)
 
-    image_size = 9
+    image_size = 38
 
     transformer = GramianAngularField(image_size)
     gaf_path = path.parent.parent / 'data' / 'gaf'
@@ -31,7 +26,7 @@ def main():
 
         # transform each trial into a GAF
         X_gaf = transformer.fit_transform(X[i])
-        X_gaf = np.stack((X_gaf, X_gaf, X_gaf), axis=0)
+        X_gaf = np.stack((X_gaf, X_gaf, X_gaf), axis=2)
 
         # save each trial as a separate numpy array
         tmp_path = gaf_path / f'trial_{i}_label_{y[i]}.npy'
