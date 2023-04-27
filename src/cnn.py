4c4a0d1471694e24272980ff32334393de0ea1a5
“laurabpauslen” <202005791@post.au.dk>
2023-04-27 18:23:44 +0200

    add balance class weights

--- src/cnn.py
+++ src/cnn.py
@@ -12,7 +12,7 @@ from sklearn.metrics import classification_report
 
 # tf tools
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import (Conv2D, 
+from tensorflow.keras.layers import (Conv3D, 
                                      Activation, 
                                      Flatten, 
                                      Dense)
@@ -24,21 +24,18 @@ def load_gafs(gaf_path):
     gafs = []
     labels = []
 
-    # loop over subjects:    
-    subjects = [x for x in gaf_path.iterdir() if x.is_dir()]
-    
-    for subject in tqdm(subjects, desc='Loading data from subjects'):
-        for file in os.listdir(os.path.join(subject, subject)):
-            if file.endswith('.npy'):
-                gaf = np.load(os.path.join(gaf_path,subject, file))
-                gafs.append(gaf)
-                label = file.split('.npy')[0]
-                label = label.split('_')[-1]
-                labels.append(label)
+    for file in gaf_path.iterdir():
+        if file.is_file():
+            gaf = np.load(file)
+            gafs.append(gaf)
+            
+            label = str(file).split('.npy')[0]
+            label = label.split('_')[-1]
+            labels.append(label)
 
     gafs = np.array(gafs)
     labels = np.array(labels)
-
+    
     return gafs, labels
 
 def prep_model():
@@ -46,17 +43,18 @@ def prep_model():
     model = Sequential()
 
     # Define CONV => ReLU
-    model.add(Conv2D(32, 
-                    (3,3),
+    model.add(Conv3D(32, 
+                    kernel_size = 6,
                     padding = "same",
-                    input_shape = (63, 26, 26, 3)))
+                    input_shape = (38, 38, 19, 3)))
+    
     model.add(Activation("relu"))
             
     # FC classifier
     model.add(Flatten())
     model.add(Dense(100))
     model.add(Activation("relu"))
-    model.add(Dense(12))
+    model.add(Dense(3))
     model.add(Activation("softmax"))
 
     # compile model
@@ -72,8 +70,8 @@ def train_model(model, X_train, y_train):
     # train model
     history = model.fit(X_train, y_train,
                         validation_split = 0.2,
-                        batch_size=32,
-                        epochs=50,
+                        batch_size=64,
+                        epochs=10,
                         verbose=1)
 
     return model, history
@@ -88,14 +86,98 @@ def test_model(model, X_test, y_test, label_names):
     
     return clf_report
 
+def plot_history(history, save_path:Path=None):
+    """
+    Plots the training history.
+
+    Parameters
+    ----------
+    history : History
+        Training history.
+    save_path : Path, optional
+        Path to save the plot to, by default None
+    
+    Returns
+    -------
+    fig : Figure
+        Figure object.
+    axes : Axes
+        Axes object.
+    """
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5), dpi = 300, sharex=True)
+
+    # plot accuracy
+    axes[0].plot(history.history["accuracy"], label='train')
+    axes[0].plot(history.history["val_accuracy"],linestyle = "--",label="val")
+    axes[0].set_title("Accuracy")
+
+    # plot loss
+    axes[1].plot(history.history['loss'], label='train')
+    axes[1].plot(history.history['val_loss'], label='val', linestyle = "--")
+    axes[1].set_title('Loss')
+
+    # add legend
+    axes[0].legend()
+    axes[1].legend()
+
+    # add labels
+    fig.supxlabel('Epoch')
+
+    plt.tight_layout()
+
+    # save plot
+    if save_path:
+        plt.savefig(save_path)
+    
+    return fig, axes
+
+def balance_class_weights(X, y, verbose = True):
+    """
+    Balances the class weight by removing trials from the class with the most trials.
+
+    Parameters
+    ----------
+    X : array
+        Data array.
+    y : array
+        Array with shape (n_trials, )
+    verbose : bool, optional
+        Print statements. The default is True.
+    
+    Returns
+    -------
+    X_equal : array
+        Data array with shape (n_channels, n_trials, n_times) with equal number of trials for each class
+    y_equal : array
+        Array with shape (n_trials, ) containing classes with equal number of trials for each class
+    """
+    keys, counts = np.unique(y, return_counts = True)
+    if counts[0]-counts[1] > 0:
+        index_inanimate = np.where(np.array(y) == 0)
+        random_choices = np.random.choice(len(index_inanimate[0]), size = counts[0]-counts[1], replace=False)
+        remove_ind = [index_inanimate[0][i] for i in random_choices]
+    else:
+        index_animate = np.where(np.array(y) == 1)
+        random_choices = np.random.choice(len(index_animate[0]), size = counts[1]-counts[0], replace=False)
+        remove_ind = [index_animate[0][i] for i in random_choices]
+
+    X_equal = np.delete(X, remove_ind, axis = 1)
+    y_equal = np.delete(y, remove_ind, axis = 0)
+
+    if verbose:
+        print(f'Removed a total of {len(remove_ind)} trials')
+        print(f'{len(y_equal)} remains')
+    
+    return X_equal, y_equal
+
 def main():
     path = Path(__file__)
     gaf_path = path.parent.parent / 'data' / 'gaf'
     gafs, labels = load_gafs(gaf_path)
 
-    # train test split
+    # train test split 
+    ## HUSK AT TJEKKE AT TRAIN TEST SPLIT ER DÅ DEN RIGTIGE DIMENSION <33
     X_train, X_test, y_train, y_test = train_test_split(gafs, labels, test_size=0.2, random_state=42)
-
     # convert labels from integers to vectors
     lb = LabelBinarizer()
     y_train = lb.fit_transform(y_train)
@@ -108,7 +190,7 @@ def main():
     model, history = train_model(model, X_train, y_train)
 
     # evaluate model
-    clf_report = test_model(model, X_test, y_test, label_names = ['clothing', 'fruits', 'plants', 'mammal', 'human', 'furniture', 'aquatic', 'insect', 'tools', 'bird', 'shapes', 'object'])
+    clf_report = test_model(model, X_test, y_test, label_names=["A", "D", "C"])
 
     report_path = path.parents[1] / 'mdl' / 'cnn.txt'
 
@@ -118,6 +200,9 @@ def main():
     # save model
     model_path = path.parents[1] / 'mdl' / 'cnn.h5'
     model.save(model_path)
+
+    # plot history
+    plot_history(history, save_path =  path.parents[1] / 'mdl' / 'history.png')
 
 
 if __name__ == '__main__':
