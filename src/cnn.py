36061a78a1b4e1e81a652309abbd97155ddc09c0
“laurabpauslen” <202005791@post.au.dk>
2023-04-30 13:38:28 +0200

    input shape and labels to match new data

--- src/cnn.py
+++ src/cnn.py
@@ -24,18 +24,15 @@ def load_gafs(gaf_path):
     gafs = []
     labels = []
 
-    for file in gaf_path.iterdir():
+    files = list(gaf_path.iterdir())
+
+    for file in tqdm(files[:4000], desc="Loading in data"):
         if file.is_file():
             gaf = np.load(file)
             gafs.append(gaf)
-            
-            label = str(file).split('.npy')[0]
-            label = label.split('_')[-1]
-            if label == 'A' or label == 'F': # if participant suffered from alzheimers or frontotemporal dementia
-                labels.append(1)
-            else:
-                labels.append(0)
-   
+
+            label = str(file)[-5]
+            labels.append(int(label))
     gafs = np.array(gafs)
     labels = np.array(labels)
     
@@ -49,7 +46,7 @@ def prep_model():
     model.add(Conv3D(32, 
                     kernel_size = 6,
                     padding = "same",
-                    input_shape = (38, 38, 19, 3),
+                    input_shape = (38, 38, 63, 3),
                     kernel_regularizer='l1'))
     
     model.add(Activation("relu"))
@@ -66,13 +63,13 @@ def prep_model():
     model.add(Flatten())
     model.add(Dense(100))
     model.add(Activation("relu"))
-    model.add(Dense(3))
+    model.add(Dense(1))
     model.add(Activation("softmax"))
 
     # compile model
     opt = SGD(learning_rate=0.01)
 
-    model.compile(loss="categorical_crossentropy", 
+    model.compile(loss="binary_crossentropy", 
                 optimizer=opt,
                 metrics=["accuracy"])
 
@@ -89,7 +86,7 @@ def train_model(model, X_train, y_train):
 
 def test_model(model, X_test, y_test, label_names):
     # evaluate model
-    predictions = model.predict(X_test, batch_size = 32)
+    predictions = model.predict(X_test)
 
     clf_report = classification_report(y_test.argmax(axis=1),
                                        predictions.argmax(axis=1),
@@ -187,11 +184,6 @@ def main():
     # train test split 
     X_train, X_test, y_train, y_test = train_test_split(gafs, labels, test_size=0.2, stratify=labels, random_state=42)
     
-    # convert labels from integers to vectors
-    lb = LabelBinarizer()
-    y_train = lb.fit_transform(y_train)
-    y_test = lb.transform(y_test)
-
     # initialize model
     model = prep_model()
 
@@ -199,7 +191,7 @@ def main():
     model, history = train_model(model, X_train, y_train)
 
     # evaluate model
-    clf_report = test_model(model, X_test, y_test, label_names=["Alzheimers or Dementia", "Control"])
+    clf_report = test_model(model, X_test, y_test, label_names=["Animate", "Inanimate"])
 
     report_path = path.parents[1] / 'mdl' / 'cnn.txt'
 
