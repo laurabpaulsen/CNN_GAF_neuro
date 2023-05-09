d1384e4dd3dcdebac02c8f40d223eb5cc1fb839f
laurabpaulsen <202005791@post.au.dk>
2023-05-09 15:36:40 +0200

    add plotting func

--- src/plot_results.py
+++ src/plot_results.py
@@ -4,6 +4,19 @@ import numpy as np
 from pathlib import Path
 
 
+# set parameters for plots
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['image.cmap'] = 'RdBu_r'
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['axes.titlesize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['legend.fontsize'] = 10
+plt.rcParams['legend.title_fontsize'] = 12
+plt.rcParams['figure.titlesize'] = 16
+plt.rcParams['figure.dpi'] = 300
+
+
 def get_accuracy(clf_report):
     # get the accuracy from the classification report string
     return float(clf_report.split('\n')[-3].split()[2])
@@ -25,16 +38,37 @@ def load_clf_reports(mdl_path):
 
     return clf_reports, subjects
 
+def plot_accuracies(accuracies, subjects, save_path=None):
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+
+    ax.bar(subjects, accuracies)
+    ax.set_ylabel('Accuracy')
+    ax.set_xlabel('Subject')
+    ax.set_title('Classification Accuracy')
+
+    # plot mean accuracy
+    ax.axhline(np.mean(accuracies), color='k', linestyle='--', label='Mean Accuracy')
+    ax.legend()
+
+    plt.tight_layout()
+
+    if save_path:
+        plt.savefig(save_path)
+
+
 def main():
     path = Path(__file__)
-
-    mdl_paths = path.parents[1] / 'mdl'
+    mdl_path = path.parents[1] / 'mdl'
+    fig_path = path.parents[1] / 'figs'
 
     # load the classification reports
-    clf_reports, subjects = load_clf_reports(mdl_paths)
+    clf_reports, subjects = load_clf_reports(mdl_path)
 
     # get the accuracies
     accuracies = get_all_accuracies(clf_reports)
+
+    # plot the accuracies
+    plot_accuracies(accuracies, subjects, save_path=fig_path / 'accuracies.png')
 
 if __name__ == '__main__':
     main()
