7af53118804e4861f61721ad0af6db40ea83a688
“laurabpauslen” <202005791@post.au.dk>
2023-05-09 15:46:01 +0200

    remove all

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ echo "[INFO]: Getting features for the CNN for all subjects  (GAFs and MTFs)"
 # python src/timeseries2gaf.py
 
 # Define the subjects
-subjects="sub-01 sub-02 sub-03 sub-04 sub-05 sub-06 sub-07 sub-08 sub-09 sub-10 sub-11 sub-12 sub-13 sub-14 sub-15 sub-16 all"
+subjects="sub-01 sub-02 sub-03 sub-04 sub-05 sub-06 sub-07 sub-08 sub-09 sub-10 sub-11 sub-12 sub-13 sub-14 sub-15 sub-16"
 
 # Iterate over each subject
 for subject in $subjects; do
