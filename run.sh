9cb107460d1af3368996740e33dc705abf76ff19
laurabpaulsen <202005791@post.au.dk>
2023-05-09 16:09:03 +0200

    add plot results

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ source env/bin/activate
 
 # Preprocess EEG data
 echo "[INFO]: Preprocessing EEG data"
-# python src/preprocess_eeg.py
+python src/preprocess_eeg.py
 
 # Get GAFs and MTFs for all subjects
 echo "[INFO]: Getting features for the CNN for all subjects  (GAFs and MTFs)"
-# python src/timeseries2gaf.py
+python src/timeseries2gaf.py
 
 # Define the subjects
 subjects="sub-01 sub-02 sub-03 sub-04 sub-05 sub-06 sub-07 sub-08 sub-09 sub-10 sub-11 sub-12 sub-13 sub-14 sub-15 sub-16"
@@ -19,3 +19,7 @@ for subject in $subjects; do
     echo "[INFO]: Running CNN for $subject"
     python src/cnntorch.py --sub $subject
 done
+
+# plot the accuracies
+echo "[INFO]: Plotting the results"
+python src/plot_results.py
